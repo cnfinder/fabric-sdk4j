fbe20f5f845a488c1beca5a40ed3f61ca9b7fc1a
finder <171721626@qq.com>
2018-03-19 16:12:52 +0800

    first

---
+++ src/main/java/cn/finder/hyperledger/fabric/App.java
@@ -0,0 +1,13 @@
+package cn.finder.hyperledger.fabric;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
